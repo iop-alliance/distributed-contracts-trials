3bd55be14e48f7e8b1c164b26fe24c2cd7289793
Max Wardeh <max@wardeh.org>
2024-10-31 12:53:43 +0100

    Update typography defaults

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -15,7 +15,20 @@ module.exports = {
     extend: {
       colors: {
         brand: "#FD4F00",
-      }
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#333',
+            a: {
+              color: '#3182ce',
+              '&:hover': {
+                color: '#2c5282',
+              },
+            },
+          },
+        },
+      },
     },
   },
   plugins: [
